9a4b59dcead87d66f7ee66755441ec434669037e
Hacker-C <1507559148@qq.com>
2021-01-22 23:56:40 +0800

    优化java目录结构

--- java/code/JavaSE/src/mphy/test/day1/Test.java
+++ java/code/JavaSE/src/mphy/test/day1/Test.java
@@ -1,7 +1,7 @@
-package mphy.test.a;
+package mphy.test.day1;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello World!");
     }
 }
