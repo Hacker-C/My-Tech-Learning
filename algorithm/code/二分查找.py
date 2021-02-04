061ffa6f0a363d98fd42353161d9a4ca7863c484
Hacker-C <1507559148@qq.com>
2021-02-04 12:46:00 +0800

    二分查找

---
+++ algorithm/code/二分查找.py
@@ -0,0 +1,13 @@
+def binnary_search(ls:list, item:int)->int:
+    low = 0
+    high = len(ls) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if ls[mid] == item:
+            return mid
+        elif ls[mid] < item:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return None
+print(binnary_search([1,2,3,4,5,6,7,8,9,10], 90))
