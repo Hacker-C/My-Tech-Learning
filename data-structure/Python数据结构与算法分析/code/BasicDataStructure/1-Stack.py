687aa984d12cbfe083a0514a5912b0af4b741269
Hacker-C <1507559148@qq.com>
2021-02-22 11:55:03 +0800

    学习python数据结构与算法

--- data-structure/Python数据结构与算法分析/code/BasicDataStructure/1-Stack.py
+++ data-structure/Python数据结构与算法分析/code/BasicDataStructure/1-Stack.py
@@ -17,8 +17,22 @@ class Stack:
     def size(self):
         return len(self.items)
 
+    def delete(self):
+        del self.items
 
-def brackets_matches(s: str) -> bool:
+    def __str__(self):
+        return '{}'.format(self.items)
+
+
+t = Stack()
+t.push(123)
+t.push('aaa')
+print(t)
+t.delete()
+# print(t)
+
+
+def brackets_matches1(s: str) -> bool:
     """
     特殊情况：一种括号
     """
@@ -32,9 +46,32 @@ def brackets_matches(s: str) -> bool:
                     return False
             else:
                 return False
-    if stk.size() != 0:
+    if not stk.isEmpty():
         return False
     return True
 
 
-print(brackets_matches('()()(())((()))((()()))'))
+# print(brackets_matches1('()()(())((()))((()()))'))
+
+def brackets_matches2(s: str) -> bool:
+    """
+    括号匹配：匹配三种类型括号
+    """
+    stk = Stack()
+    for i in s:
+        if i == "(" or i == "[" or i == "{":
+            stk.push(i)
+        else:  # ) ] }
+            if stk.isEmpty():
+                return False
+            temp = stk.pop()
+            if not ((i == ")" and temp == "(")
+                    or (i == "]" and temp == "[")
+                    or (i == "}" and temp == "{")):
+                return False
+    if not stk.isEmpty():
+        return False
+    return True
+
+
+# print(brackets_matches2("(){}(())[][{()}][({})]()[]{}()[{}]()[()]()()"))
