0b6060a06629558e47926e5780497a81ff8d2883
Hacker-C <1507559148@qq.com>
2021-02-23 15:18:27 +0800

    优化文件结构

--- data-structure/Python数据结构与算法分析/code/test.py
+++ data-structure/Python数据结构与算法分析/code/test.py
@@ -1,3 +1,19 @@
-import string
+m, n = map(int, input().split())
+ac = [0] * (n + 1)
+u_ac = [0] * (n + 1)
+lst = []
+for j in range(m):
+    a, op = input().split()
+    a = int(a)
+    if op == 'AC':
+        ac[a] += 1
+    else:
+        u_ac[a] += 1
+    for i in range(1, n + 1):
+        if i not in lst:
+            if ac[i] == 0 and u_ac[i] == 0:
+                pass
+            elif ac[i] / (u_ac[i] + ac[i]) >= 0.5:
+                print(i, end=' ')
+                lst.append(i)
 
-print(string.ascii_uppercase)
