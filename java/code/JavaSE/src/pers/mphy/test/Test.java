c35ea56f45f29c0c0d858298a9b4159a09f4ba92
Hacker-C <1507559148@qq.com>
2021-03-01 20:00:28 +0800

    子元素绝对定位父元素相对定位

--- java/code/JavaSE/src/pers/mphy/test/Test.java
+++ java/code/JavaSE/src/pers/mphy/test/Test.java
@@ -1,8 +1,13 @@
 package pers.mphy.test;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        final double PI = 3.14159;
+        double radious = input.nextDouble();
+        double ans = PI * Math.pow(radious, 2);
+        System.out.println("The answer is " + ans);
     }
 }
 
