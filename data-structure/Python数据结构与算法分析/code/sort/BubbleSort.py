28f4bb93ab32f47555435ab1e6881c6b5cef9c00
Hacker-C <1507559148@qq.com>
2021-03-02 15:59:00 +0800

    bubble sort

---
+++ data-structure/Python数据结构与算法分析/code/sort/BubbleSort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    l = len(arr)
+    for i in range(l-1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+if __name__ == "__main__":
+    test = [34, 123, 3, 13, 5, 19, -3, 0, 37]
+    print(bubble_sort(test))
