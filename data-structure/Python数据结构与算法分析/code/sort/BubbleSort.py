39764d9aabea89d89480c6dde51a63e872274b75
Hacker-C <1507559148@qq.com>
2021-03-02 20:15:20 +0800

    土豆案例完成

--- data-structure/Python数据结构与算法分析/code/sort/BubbleSort.py
+++ data-structure/Python数据结构与算法分析/code/sort/BubbleSort.py
@@ -1,6 +1,6 @@
 def bubble_sort(arr):
     l = len(arr)
-    for i in range(l-1, 0, -1):
+    for i in range(l - 1, 0, -1):
         for j in range(i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
