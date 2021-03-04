c1ef5f8be1c5978c59907df688b5fa4de9724839
Hacker-C <1507559148@qq.com>
2021-03-04 16:24:44 +0800

    JS数字类型

---
+++ front-end/JS/code/07.js
@@ -0,0 +1,10 @@
+var b = 0b11
+var o = 011
+var h = 0x11
+console.log(b,o,h);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
