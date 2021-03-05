7a288e099e8ecaa6e31fdc875597b9613a4125be
Hacker-C <1507559148@qq.com>
2021-03-05 23:55:35 +0800

    JS

---
+++ front-end/JS/code/JS基础第二天/01.js
@@ -0,0 +1,5 @@
+var strNum = 123
+console.log(strNum, typeof strNum);
+console.log(strNum.toString(), typeof strNum.toString());
+console.log(String(strNum), typeof String(strNum));
+console.log(strNum + '', typeof (strNum + ''));
---
+++ front-end/JS/code/JS基础第二天/02.js
@@ -0,0 +1,9 @@
+console.log(parseInt('123')); // 123
+console.log(parseFloat('3.14')); // 3.14
+console.log('123' - 0); // 123
+console.log('123' - ''); // 123
+console.log('1.23' - 0); // 1.23
+console.log('1.23' - ''); // 1.23
+console.log(parseFloat('999')); // 999
+console.log(parseInt('3.14159')); // 3
+console.log(parseInt('120px')); // 120
