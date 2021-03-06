9178b38d9db90de34df63516503742c0fc3bd8d5
Hacker-C <1507559148@qq.com>
2021-03-06 10:46:21 +0800

    JS数字类型转换

--- front-end/JS/code/JS基础第二天/02.js
+++ front-end/JS/code/JS基础第二天/02.js
@@ -2,8 +2,15 @@ console.log(parseInt('123')); // 123
 console.log(parseFloat('3.14')); // 3.14
 console.log('123' - 0); // 123
 console.log('123' - ''); // 123
-console.log('1.23' - 0); // 1.23
-console.log('1.23' - ''); // 1.23
 console.log(parseFloat('999')); // 999
 console.log(parseInt('3.14159')); // 3
 console.log(parseInt('120px')); // 120
+console.log(Number('100')); // 100
+console.log(Number('100.32')); // 100.32
+console.log(Number('100px')); // NaN
+console.log('100px' - ''); // NaN
+
+console.log('10' + '2'); // 102
+console.log('10' - '2'); // 8
+console.log('10' + '3.2'); // 103.2
+console.log('10' - '3.2'); // 6.8
