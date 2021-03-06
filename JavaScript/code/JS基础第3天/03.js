0f728c9a8b172608cd4948c818f2e3348b999e71
Hacker-C <1507559148@qq.com>
2021-03-06 16:31:11 +0800

    JS

--- JavaScript/code/JS基础第3天/03.js
+++ JavaScript/code/JS基础第3天/03.js
@@ -1,4 +1,10 @@
-var arr = new Array(1,2,3,4,5);
-for (let i in arr) {
-    console.log(i+'.');
+function fibonacci(n) {
+    var a = [0, 1, 1];
+    if (n >= 3) {
+        for (var i = 3; i <= n; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+    }
+    return a[n];
 }
+console.log(fibonacci(5));
